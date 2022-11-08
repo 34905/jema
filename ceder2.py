6f0cd7b3fdfb42f96e702c13a35b8a98c94b9d04
34905 <114576132+34905@users.noreply.github.com>
2022-11-08 14:45:05 +0100

    Add files via upload

---
+++ ceder2.py
@@ -0,0 +1,168 @@
+print("Het is winer en om presies te zijn winter 2022")
+input()
+print("De oorlog escaleert tussen de Russen en  de nato,Rusland valt de Baltische staten aan")
+print("Het is nu officeel de 3e weereld oorlog wat doe je nu?")
+input()
+print("A.Je gaat het leger in om je land te bescheremen ")
+print("B.Je gaat proberen te vluchten naar een land waar er volgerns jou geen oorlog zou komen ")
+print("C.Je gaat onderduiken ")
+print("D.Je start een revolutsie")
+choice = input()
+if choice == "A":
+    print("Je ben al 18 bij de tijd dat de oorlog begint dus wat betekend dat je voor het leger kan inschrijven ")
+    input()
+    print("Na dat je aplicatie is binnen gekomen en je goed bent gekeurt kan je beginen in het leger")
+    print("En daar sta je dan met een geweer in je hand oog tot oog met de vijhand en moet nu de beslising maken ")
+    print("A. Je schiet hem gelijk ")
+    print("B. Je schiet hem niet ")
+
+    choice = input()
+
+    if choice == "A":
+        print("Daar sta de dan er gaan veel emoties door je heen maar een luid het hardst is dat jij een mensen zijn leven heb genomen")
+        print("Jouw team heeft de strijd gewonen of is dat zo want jij ziet van het zuiden 15 man naar jullie toe komen ")
+        input()
+        print("Je pakt je granaat er bij je totale moorden zijn tot dit moment 3 mensen heb je het wel in je om in een keer 15 mensen te doden?")
+        print("A. Je gooid de granaat wel ")
+        print("B. Je gooid de granaat niet ")
+        choice = input()
+
+        if choice == "A":
+            print("Je weet niet hoe je je moet voelen...")
+            print("De oorlog is grotendeels door jou gewonen en je wordt uitgerijkt tot internatsionale held")
+            input()
+            print("Je hebt de oorlog overleeft maar je het een zware PTSD opgelopen")
+
+        elif choice == "B":
+            print("Wat heb je nou gedaan jij en je team zijn afgesclacht ")
+            input()
+            print("Door jou keuze ging de oorlog 2 jaar langer door en stierven er een extra 8 miljoen mensen")
+
+
+    elif choice == "B":
+        print("Wat heb je nou gedaan jij en je team zijn afgesclacht")
+        input()
+        print("Door jou keuze ging de oorlog 2 jaar langer door en stierven er een extra 8 miljoen mensen")
+
+
+
+if choice == "B":
+    print("Je pakt alles wat je heb ook al is het niet veel")
+    input()
+    print("na te hebben een nadenk sesie besluit je om naar het midden ooste te gaan")
+    print("maar sins de commerciele vliegtuigen niet meer vliegen moet je over land hoe ga je?")
+    print("A. Je gaat door Noord Africa")
+    print("B. Je gaat door de Mediterranean Sea ")
+    choice = input()
+
+    if choice == "A":
+        print("Zo het word Noord Africa dus oke!")
+        input()
+        print("Zo noor noord africa gaat niet zo 1 2 3 natuurlijk")
+        print("Maar je vind een groep Marokane die offeren om jouw mee te nemen met hun naar marokko")
+        input()
+        print("Uiteindelijk ben je aangekomen in marokko maar er word aan je gevraagd om te blijven!")
+        print("A. je blijft in marokko ")
+        print("B. je zet je plan door en je gaat naar het midden oosterse land Iraq ")
+        choice = input()
+
+        if choice == "A":
+            print("Je bent gebleven in marokko waar de oorlog verassend genoeg niet was gekomen!!!")
+            print("uiteindelijk heb een een familie gestart en werk je een schilder")
+
+        elif choice == "B":
+            print("Je bent een paar maanden bezig door de oorlog zijn de landen waar je door heen moet in paniek")
+            print("Maar uiteindelijk ben je daar aangekomen maar je overleeft het niet lang omdat je gezonheid slecht was geworden")
+        
+    elif choice == "B": 
+        print("Mediterranean Sea oke dat is geen slechte keus")
+        input()
+        print("Je krijgt een herinering van je oom waarmeer je naar italie was gefietst")
+        print("En daar kwam het iedee om via itale gelijk naar Egypte te gaan")
+        input("En verasend genoeg dat was geluk om door de internatsionale wateren te gaan en je bent nu in Egypte")
+        print("A. Je blijft in Egypte")
+        print("B. je zet je plan door en je gaat naar het midden oosterse land Oman ")
+        choice = input()
+
+        if choice == "A":
+            print("Egypte bleek er helaas genoeg oorlog te zijn")
+            print("En in alle gaos en terror was je omgekomen in een aanslag")
+            choice = input()
+
+        if choice == "B":
+            print("Er was onder weg moeite door de oorlog in Egypte maar het is gelukt je bent gezond en all in Oman")
+            print("Na de oorlog heb je een boek geschreven over je verhalen")
+
+
+
+
+        
+if choice == "C":
+    print("Onderduiken is iets what je wel moet doen je wilt zo min mogelijk risico om aan geweld te doen of zien")
+    input()
+    print("Maar alleen onderduiken is saai en wel te verglijken met het hell helemaal alleen")
+    input()
+    print("Dus je zoekt een groep mensen die ook gaan onderduiken ejn die vind je na een tijd")
+    input()
+    print("jullie zitten all voor 4 jaar ondergedoken het vechten is vermindert maar nog  zeer zeker actief")
+    print("A. je gaat niet meer onderduiken ")
+    print("B. je blijft ondergedoken")
+    choice = input()
+
+    if choice == "A":
+        print("De oorlog was nog in volle vors blijkbaar en je raakte slagtofer en stierf")
+
+    elif choice == "B":
+        print("Gefeciteerd je hebt het overleeft er is geen oorlog meer maar de wereld is wel op ze laatse benen maar je hebt het overleeft")
+
+
+if choice == "D":
+    print("Jij kan er niet meer tegen al dat geweld je vind dat het moet stoppen nu!!!")
+    input()
+    print("Maar je bent niet dom jij gaat niet veel in je eentje kunnen doen daarom, gaat naar vrienden om rond te geven dat wij het volk terug gaan vechten")
+    input()
+    print("Na een paar maanden heeft jouw organizatie 13000 leden te weten kunnen krijgen")
+    input()
+    print("Maar na een jaar is het geld eten en drinken bijna op maar er zijn opties niet allemaal wat je wilt maar jij bent hun leider")
+    input()
+    print("Het is nu zo ver wat ga je doen?")
+    print("A. Sluit je aan met andere verzet groepen ")
+    print("B. Ga met all je leden de laatste aanval plegen ")
+    print("C. sluit je aan bij de vijhand")
+    print("D. Niets veranderen en hopen op geluk")
+    choice = input()
+
+    if choice == "A":
+        print("Er komt een ontmoeting tussen jouw en de lijder van de andere groep")
+        print("Jullie besluiten om samen te werken maar nu de aanval strategie")
+        print("A.jullie vallen aan in de nacht")
+        print("B jullie vallen aan in de ochtend")
+
+        choice = input()
+
+        if choice == "A":
+            print("De aanval was sucsesful en jullie hadden de oorlog gewonen")
+
+        elif choice == "B":
+            print("Dat was de foute keuze helaas jullie hadden hart verloren en worden afgemaakt")
+
+    elif choice == "B":
+        print("Jullie vallen allemaal en sterven met meer en faam en worden herdenk als helden")
+
+    elif choice == "C":
+        print("Je organizatie split maar in jouw gevalt ben je blij dat ze veilg zijn ")
+        print("Na goed gedrach enhard werk heb je een mogelijk hij op een hoge positsie")
+        print("Maar er is kans dat je de afgesplitste devisie moet gaan vechten")
+        print("A.Je neemt de positsie")
+        print("B Je neemt de positsie niet")
+        choice = input()
+
+        if choice == "A":
+            print("het werkte allemaal uit jij hoeften je ouden frienden niet te moorden en jullie leven allemaal")
+
+        elif choice == "B":
+            print("je wordt verklaart als spy en wordt geelimineert")
+
+
+    elif choice == "D":
+        print("Het is helaas niet goed gekomen zonder wapen of voedsel hadden jullie de oorlog verloren")
